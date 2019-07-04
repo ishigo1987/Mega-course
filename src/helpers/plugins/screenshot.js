8bb971373dc975ab0625bf97c705d4bece4ffe71
ishigo1987 <stephnguets@yahoo.fr>
2019-07-04 11:27:31 +0100

    Update

--- src/helpers/plugins/screenshot.js
+++ src/helpers/plugins/screenshot.js
@@ -5,7 +5,7 @@ module.exports = ()=>{
        console.error(error);
      }else{
        console.log('ok',res.filePath);
-       resolve(path:res.filepath);
+       resolve({path:res.filepath});
      }
     },'jpg',100,'megaCourse');
   });
